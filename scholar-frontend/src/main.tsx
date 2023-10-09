f948cbe2462fb0c6066db1ca5f28bf275495b773
angelo.yanve <angelo.yanve@mehriso.com>
2023-10-09 14:32:33 +0100

    feat: add tailwindcss to frontend

--- scholar-frontend/src/main.tsx
+++ scholar-frontend/src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import App from './App.js'
+import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <h1 className='text-red-800'>Hello</h1>
     <App />
   </React.StrictMode>,
 )
